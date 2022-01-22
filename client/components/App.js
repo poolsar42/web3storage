b250b87e0950ccef04d124f88fd25d8c25eafd05
emil <poolsar.42@gmail.com>
2022-01-23 00:09:20 +0300

    minor changes

--- client/components/App.js
+++ client/components/App.js
@@ -1,33 +1,13 @@
 import React, { useState } from "react";
 import "./App.css";
 import DropZone from "./DropZone";
-import { Web3Storage, getFilesFromPath } from "web3.storage";
 
 const App = (props) => {
-  const [files, setFiles] = useState([]);
-
-  const makeFileObject = (file) => {
-    // You can create File objects from a Blob of binary data
-    // see: https://developer.mozilla.org/en-US/docs/Web/API/Blob
-    // Here we're just storing a JSON object, but you can store images,
-    // audio, or whatever you want!
-    return new File([file], file.path.split("/")[-1]);
-  };
-
-  async function storeFiles(event) {
-    event.preventDefault();
-    // const path = files[0].path.split("/").slice(0, -1).join("/");
-    // const _files = await getFilesFromPath(path);
-    console.log(files);
-    // const cid = await client.put(_files);
-    // console.log(_files[0]);
-  }
-
   return (
     <div>
       <p className="title">React Drag and Drop Image Upload</p>
-      <DropZone setFiles={setFiles} />
-      <form className="btn" onSubmit={storeFiles}>
+      <DropZone />
+      <form className="btn">
         <button>Upload to IPFS</button>
       </form>
     </div>
--- client/components/DropZone.js
+++ client/components/DropZone.js
@@ -1,20 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./DropZone.css";
+import { Web3Storage, getFilesFromPath } from "web3.storage";
 import { useDropzone } from "react-dropzone";
 
-const DropZone = ({ setFiles }) => {
+const DropZone = (props) => {
   const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
+  const [_files, setFiles] = useState([]);
 
   const files = acceptedFiles.map((file) => (
-    <li key={file.path}>
-      <img src={file.path} />
-      {JSON.stringify(file)}
-    </li>
+    <li key={file.path}>{file.path}</li>
   ));
 
-  if (acceptedFiles.length) {
-    console.log(makeFileObject(acceptedFiles[0]));
-    setFiles(acceptedFiles);
+  const makeFileObject = (file) => {
+    // You can create File objects from a Blob of binary data
+    // see: https://developer.mozilla.org/en-US/docs/Web/API/Blob
+    // Here we're just storing a JSON object, but you can store images,
+    // audio, or whatever you want!
+    return new File([file], file.path.split("/")[-1]);
+  };
+
+  async function storeFiles(event) {
+    event.preventDefault();
+    // const path = files[0].path.split("/").slice(0, -1).join("/");
+    // const _files = await getFilesFromPath(path);
+    console.log(files);
+    // const cid = await client.put(_files);
+    // console.log(_files[0]);
   }
 
   return (
@@ -31,4 +42,4 @@ const DropZone = ({ setFiles }) => {
     </>
   );
 };
-export default DropZone;
+export default React.memo(DropZone);
