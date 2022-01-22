c040db8aabd7e9c3afbf1b02c3e1546e697d9c16
emil <poolsar.42@gmail.com>
2022-01-22 17:28:24 +0300

    minor changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
-  entry: ["./client/index.js"],
+  entry: ["@babel/polyfill", "./src/index.js"],
   output: {
     path: path.resolve(__dirname, "public"),
     publicPath: "/public/",
@@ -16,9 +16,32 @@ module.exports = {
         exclude: /node_modules/,
         loader: "babel-loader",
         options: {
-          presets: ["@babel/preset-react"],
+          presets: ["@babel/preset-react", "@babel/preset-env"],
         },
+      },
+      {
+        test: /\.css$/i,
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
       },
     ],
   },
+  // * IN CASE I WANT TO DO STUFF MANUALLY
+  // resolve: {
+  //   alias: {
+  //     url: require.resolve("url/"),
+  //     https: require.resolve("https-browserify"),
+  //     http: require.resolve("stream-http"),
+  //     crypto: require.resolve("crypto-browserify"),
+  //     assert: require.resolve("assert/"),
+  //     stream: require.resolve("stream-browserify"),
+  //   },
+  // },
 };
